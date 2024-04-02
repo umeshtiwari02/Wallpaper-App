7351327e62fb8740edc1f8b49f7c5e3599554e2c
Umesh Tiwari <klau.tiwari1234@gmail.com>
2024-04-02 15:12:37 +0530

    packages are updated

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -37,6 +37,7 @@ class _HomePageState extends State<HomePage> {
                       fontWeight: FontWeight.bold,
                       fontFamily: 'Poppins'),
                 ),
+                
               ],
             )
           ],
