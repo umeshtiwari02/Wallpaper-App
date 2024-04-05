0656605f44d586cc6bf727ff0def5edcf5e0b21f
Umesh Tiwari <klau.tiwari1234@gmail.com>
2024-04-05 09:33:41 +0530

    category page is updated

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -11,41 +11,181 @@ class _CategoriesState extends State<Categories> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.only(top: 50),
-        child: Column(
-          children: [
-            Center(
-              child: Text(
-                "Categories",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Poppins'),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.only(top: 50),
+          child: Column(
+            children: [
+              const Center(
+                child: Text(
+                  "Categories",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Poppins'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
+              const SizedBox(
+                height: 20.0,
               ),
-              child: Stack(
-                children: [
-                  Image.asset(
-                    "assets/images/w1.jpg",
-                    width: MediaQuery.of(context).size.width,
-                    height: 180,
-                    fit: BoxFit.cover,
-                  ),
-                ],
+              Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        "assets/images/w1.jpg",
+                        width: MediaQuery.of(context).size.width,
+                        height: 180,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 180,
+                      decoration: BoxDecoration(
+                        color: Colors.black26,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Center(
+                        child: Text(
+                            "WildLife",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Poppins',
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                      ),
+                      ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 20,),
+              Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        "assets/images/w2.jpg",
+                        width: MediaQuery.of(context).size.width,
+                        height: 180,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 180,
+                      decoration: BoxDecoration(
+                        color: Colors.black26,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Center(
+                        child: Text(
+                            "Flower",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Poppins',
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                      ),
+                      ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20,),
+              Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        "assets/images/w3.jpeg",
+                        width: MediaQuery.of(context).size.width,
+                        height: 180,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 180,
+                      decoration: BoxDecoration(
+                        color: Colors.black26,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Center(
+                        child: Text(
+                            "Nature",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Poppins',
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                      ),
+                      ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20,),
+              Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        "assets/images/w4.jpeg",
+                        width: MediaQuery.of(context).size.width,
+                        height: 180,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 180,
+                      decoration: BoxDecoration(
+                        color: Colors.black26,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Center(
+                        child: Text(
+                            "GreenLand",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Poppins',
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                      ),
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
